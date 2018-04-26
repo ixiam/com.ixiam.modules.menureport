c7f6e2aa974fbfb0e96cb7a2b62b3bd60ca79d91
Ruben <r.pineda@ixiam.com>
2018-04-26 14:04:46 +0200

    Allow sort by user, and add class even/odd

--- api/v3/Reportlistorder.php
+++ api/v3/Reportlistorder.php
@@ -4,12 +4,14 @@
  * In civicrm 4.6 don't work correctly the api setting
  */
 function civicrm_api3_reportlistorder_get($params) {
-  return civicrm_api3_create_success(CRM_Core_BAO_Setting::getItem('reportlistsorter', 'reportlist'), $params, 'reportlistorder', 'get');
+  $contact_id = CRM_Core_Session::singleton()->getLoggedInContactID();
+  return civicrm_api3_create_success(CRM_Core_BAO_Setting::getItem('reportlistsorter', 'reportlist.' . $contact_id), $params, 'reportlistorder', 'get');
 }
 
 
-function civicrm_api3_reportlistorder_set($params) {  
-  CRM_Core_DAO::executeQuery("DELETE FROM `civicrm_setting` WHERE `group_name` = 'reportlistsorter' AND `name` = 'reportlist'"); //Some times is writed two times
-  CRM_Core_BAO_Setting::setItem($params["value"], 'reportlistsorter', 'reportlist');
+function civicrm_api3_reportlistorder_set($params) {
+  $contact_id = CRM_Core_Session::singleton()->getLoggedInContactID();
+  CRM_Core_DAO::executeQuery("DELETE FROM `civicrm_setting` WHERE `group_name` = 'reportlistsorter' AND `name` = 'reportlist." . $contact_id . "'"); //Some times is writed two times
+  CRM_Core_BAO_Setting::setItem($params["value"], 'reportlistsorter', 'reportlist.' . $contact_id);
   return civicrm_api3_create_success(true,$params, 'reportlistorder', 'set');
 }
