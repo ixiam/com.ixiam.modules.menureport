0044084051d8c0f258427fd02f976df04ea0d87e
Cesar <c.jimenez@ixiam.com>
2019-02-01 18:30:08 +0100

    Adapt to v5.9

--- menureport.civix.php
+++ menureport.civix.php
@@ -269,10 +269,10 @@ function _menureport_civix_civicrm_managed(&$entities) {
       if (empty($e['module'])) {
         $e['module'] = E::LONG_NAME;
       }
-      $entities[] = $e;
       if (empty($e['params']['version'])) {
         $e['params']['version'] = '3';
       }
+      $entities[] = $e;
     }
   }
 }
@@ -352,8 +352,10 @@ function _menureport_civix_glob($pattern) {
  * Inserts a navigation menu item at a given place in the hierarchy.
  *
  * @param array $menu - menu hierarchy
- * @param string $path - path where insertion should happen (ie. Administer/System Settings)
- * @param array $item - menu you need to insert (parent/child attributes will be filled for you)
+ * @param string $path - path to parent of this item, e.g. 'my_extension/submenu'
+ *    'Mailing', or 'Administer/System Settings'
+ * @param array $item - the item to insert (parent/child attributes will be
+ *    filled for you)
  */
 function _menureport_civix_insert_navigation_menu(&$menu, $path, $item) {
   // If we are done going down the path, insert menu
